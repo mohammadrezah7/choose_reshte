f89a94fc7d723b4943a0dccac347bb8328765a57
mohammadreza <m.r.hassani8585@gmail.com>
2026-08-08 15:34:10 +0330

    views.py fixed

--- accounts/views.py
+++ accounts/views.py
@@ -4,6 +4,7 @@ from django.contrib.auth import login
 from .forms import RegisterForm
 from .models import Profile
 from django.http import HttpResponse
+from django.contrib.auth import authenticate
 
 
 def register_view(request):
@@ -21,8 +22,37 @@ def register_view(request):
             )
             Profile.objects.create(user=user)
             login(request, user)
-            return redirect("/")
+            return redirect("accounts/signup.html")
 
     else:
         form = RegisterForm()
-    return Httpresponse("hello")
+    return render(request, "accounts/signup.html")
+
+
+def login_view(request):
+    if request.method == "POST":
+        username = request.POST.get("username")
+        password = request.POST.get("password")
+
+        user = authenticate(
+            request,
+            username=username,
+            password=password
+        )
+
+        if user is not None:
+            login(request, user)
+            return redirect("dashboard")
+
+        return render(
+            request,
+            "accounts/login.html",
+            {
+                "error": "نام کاربری یا رمز عبور اشتباه است."
+            }
+        )
+
+    return render(
+        request,
+        "accounts/login.html"
+    )
