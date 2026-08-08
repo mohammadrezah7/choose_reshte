7c12107628fc3071058c431278feed164188597a
mohammadreza <m.r.hassani8585@gmail.com>
2026-08-08 16:30:53 +0330

    logout url added

--- accounts/urls.py
+++ accounts/urls.py
@@ -4,5 +4,6 @@ from . import views
 
 urlpatterns = [
     path('login/', views.login_view, name='login'),
-    path('signup/', views.register_view, name='signup')
+    path('signup/', views.register_view, name='signup'),
+    path("logout/", views.logout_view, name="logout"),
 ]
