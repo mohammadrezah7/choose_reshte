7592d95e17f33935b14284bac521478ac4369ef7
mohammadreza <m.r.hassani8585@gmail.com>
2026-08-08 15:39:42 +0330

    urls.py fixed

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,6 +1,8 @@
 from django.contrib import admin
 from django.urls import path
+from . import views
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    path('login/', views.login_view, name='login'),
+    path('signup/', views.register_view, name='signup')
 ]
